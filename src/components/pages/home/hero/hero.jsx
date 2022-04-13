49686e58c5d38492b625aa68c90cf1bffdfcd640
Thi Van Le <vannnyle@gmail.com>
2022-04-13 16:34:21 +0300

    feat: more paddings between text

--- src/components/pages/home/hero/hero.jsx
+++ src/components/pages/home/hero/hero.jsx
@@ -8,28 +8,16 @@ const isBrowser = typeof window !== 'undefined';
 const ibmplexmonoFont =
   isBrowser && new FontFace('IBM Plex Mono', 'url(/fonts/ibmplexmono-light.woff2)');
 
-const getPixelRatio = (context) => {
-  const backingStore =
-    context.backingStorePixelRatio ||
-    context.webkitBackingStorePixelRatio ||
-    context.mozBackingStorePixelRatio ||
-    context.msBackingStorePixelRatio ||
-    context.oBackingStorePixelRatio ||
-    context.backingStorePixelRatio ||
-    1;
-
+const getPixelRatio = () => {
   const isDevicePixelRatio = isBrowser && window.devicePixelRatio;
 
-  return (isDevicePixelRatio || 1) / backingStore;
+  return isDevicePixelRatio || 1;
 };
 
 function getRandomString(length) {
+  const digits = 'ABCDEF0123456789';
   let string = '';
-  for (
-    string;
-    string.length < length;
-    string += 'ABCDEF0123456789'.charAt(Math.random() * 10 || 0)
-  );
+  for (string; string.length < length; string += digits.charAt(Math.random() * digits.length || 0));
   return string;
 }
 
@@ -41,7 +29,7 @@ const Hero = () => {
     const canvas = ref.current;
     const context = canvas.getContext('2d');
 
-    const ratio = getPixelRatio(context);
+    const ratio = getPixelRatio();
     const cvWidth = width * ratio;
     const cvHeight = height * ratio;
     canvas.width = cvWidth;
@@ -60,7 +48,7 @@ const Hero = () => {
         for (let j = 0; j < x; j += 1) {
           context.font = '300 16px IBM Plex Mono';
           context.fillStyle = '#E3E7E8';
-          context.fillText(getRandomString(2), 22 + j * 34, 22 + i * 28, width);
+          context.fillText(getRandomString(2), 22 + j * 34, 16 + i * 28);
         }
       }
     });
@@ -69,24 +57,22 @@ const Hero = () => {
   return (
     <section className="safe-paddings relative flex h-screen w-screen items-center justify-center overflow-hidden sm:justify-start">
       <canvas className="absolute -z-10" ref={ref} />
-      <div className="container-max sm:mx-0">
-        <h1 className="flex flex-col space-y-2.5 text-[38px] font-extrabold uppercase leading-none lg:space-y-1.5 lg:text-3xl lg:leading-none md:space-y-1 md:text-2xl md:leading-none sm:space-y-0.5 sm:text-[22px] sm:leading-none ">
-          <span className="mr-auto bg-white">Track</span>
-          <span className="bg-white text-[8.6rem] lg:text-[106px] md:text-[80px] sm:inline-flex sm:flex-col sm:text-[68px] xs:text-6xl xs:leading-none">
+      <div className="container-max flex flex-col sm:mx-0">
+        <h1 className="flex flex-col text-[38px] font-extrabold uppercase leading-none lg:text-3xl lg:leading-none md:text-2xl md:leading-none sm:text-[22px] sm:leading-none ">
+          <span className="mr-auto bg-white px-2.5 py-2.5">Track</span>
+          <span className="-my-1.5 bg-white px-2.5 text-[8.6rem] lg:text-[106px] md:text-[80px] sm:inline-flex sm:flex-col sm:text-[68px] xs:text-6xl xs:leading-none">
             <span>
               Memory<span className="sm:hidden">.</span>
             </span>
             <span>CPU.IO.</span>
           </span>
-          <span className="ml-auto bg-white sm:mr-auto sm:ml-0">Bottlenecks</span>
+          <span className="ml-auto bg-white px-2.5 py-2.5 sm:mr-auto sm:ml-0">Bottlenecks</span>
         </h1>
-        <Button
-          className="mt-24 lg:mt-[60px] md:mt-10 sm:mt-[84px]"
-          size="2xl"
-          theme="violet-gradient-filled"
-        >
-          Try it Now
-        </Button>
+        <div className="mt-24 self-start bg-white p-2.5 lg:mt-[60px] md:mt-10 sm:mt-[84px]">
+          <Button size="2xl" theme="black-filled">
+            Try it Now
+          </Button>
+        </div>
       </div>
     </section>
   );
