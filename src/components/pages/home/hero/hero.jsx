dfd083882b09e942381ee6f2791b2bdab425b1d3
Maksim Gorodov <silencer.web@gmail.com>
2022-04-19 16:18:49 +0400

    feat: change animation of the button in the section Hero

--- src/components/pages/home/hero/hero.jsx
+++ src/components/pages/home/hero/hero.jsx
@@ -2,6 +2,7 @@ import { motion, useAnimation } from 'framer-motion';
 import React, { useEffect, useRef } from 'react';
 import { useInView } from 'react-intersection-observer';
 
+import Link from 'components/shared/link';
 import useWindowSize from 'hooks/use-window-size';
 
 import Arrow from './arrow';
@@ -12,24 +13,23 @@ const buttonGradientVariants = {
   initial: {
     opacity: 0,
   },
-  animate: {
-    opacity: [1, 0, 0, 0, 0.5, 1],
+  animate1: {
+    opacity: 1,
     transition: {
-      duration: 0.18,
+      duration: 0.03,
     },
   },
-};
-
-const buttonTextColorVariants = {
-  initial: {
-    opacity: 1,
-    color: '#181A1B',
-  },
-  animate: {
-    opacity: [1, 0, 0, 0, 0.5, 1],
-    color: '#fff',
+  animate2: {
+    opacity: 0,
     transition: {
-      duration: 0.18,
+      duration: 0.03,
+    },
+  },
+  animate3: {
+    opacity: 1,
+    transition: {
+      duration: 0.06,
+      delay: 0.06,
     },
   },
 };
@@ -44,7 +44,6 @@ const Hero = () => {
   const arrowLineControls = useAnimation();
   const arrowPointerControls = useAnimation();
   const buttonGradientControls = useAnimation();
-  const buttonTextColorControls = useAnimation();
 
   useEffect(() => {
     const fn = async () => {
@@ -52,21 +51,13 @@ const Hero = () => {
         await titleControls.start('animate');
         await arrowLineControls.start('animate');
         await arrowPointerControls.start('animate');
-        await Promise.all([
-          buttonGradientControls.start('animate'),
-          buttonTextColorControls.start('animate'),
-        ]);
+        await buttonGradientControls.start('animate1');
+        await buttonGradientControls.start('animate2');
+        await buttonGradientControls.start('animate3');
       }
     };
     fn();
-  }, [
-    titleControls,
-    arrowLineControls,
-    arrowPointerControls,
-    buttonGradientControls,
-    buttonTextColorControls,
-    inView,
-  ]);
+  }, [titleControls, arrowLineControls, arrowPointerControls, buttonGradientControls, inView]);
 
   useEffect(() => {
     if (inView) {
@@ -89,24 +80,23 @@ const Hero = () => {
           />
         </div>
         <div className="mt-20 self-start bg-black p-2.5 lg:mt-[50px] md:mt-5 sm:mt-16 sm:px-4 xs:mt-8 xs:pr-1.5">
-          <motion.a
+          <Link
             className="relative inline-flex items-center bg-white px-[60px] py-[26px] text-[22px] font-medium leading-none transition-colors duration-200 lg:py-6 lg:px-16 lg:text-lg lg:leading-none md:py-4.5 md:px-12"
-            href="#"
-            initial="initial"
-            animate={buttonTextColorControls}
-            variants={buttonTextColorVariants}
+            to="/"
           >
+            <span>Try it Now</span>
             <motion.span
-              className="absolute top-0 left-0 right-0 bottom-0 h-full w-full"
+              className="absolute top-0 left-0 right-0 bottom-0 flex h-full w-full items-center justify-center text-white"
               style={{
                 backgroundImage: 'linear-gradient(264.04deg, #F14AFF 15.67%, #401AFF 82.95%)',
               }}
               initial="initial"
               animate={buttonGradientControls}
               variants={buttonGradientVariants}
-            />
-            <span className="relative">Try it Now</span>
-          </motion.a>
+            >
+              Try it Now
+            </motion.span>
+          </Link>
         </div>
       </div>
     </section>
