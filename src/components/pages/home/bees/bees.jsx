e97d091d699a58ee585a1ca365cbca31ef965382
Maksim Gorodov <silencer.web@gmail.com>
2022-04-19 19:21:43 +0400

    feat: add bee animation in the section EBPF

--- src/components/pages/home/bees/bees.jsx
+++ src/components/pages/home/bees/bees.jsx
@@ -1,19 +1,23 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { useInView } from 'react-intersection-observer';
 
 import EBPF from '../ebpf';
 import Features from '../features';
 
 const Bees = () => {
-  const sectionRef = useRef(null);
+  const [sectionRefFunction, isSectionInView] = useInView();
+  const sectionRef = useRef();
 
   const [scrollPosition, setScrollPosition] = useState(0);
 
   const [animationStartPosition, setAnimationStartPosition] = useState(0);
   const [animationEndPosition, setAnimationEndPosition] = useState(0);
 
-  const handleScroll = useCallback(() => {
-    setScrollPosition(window.pageYOffset);
-  }, []);
+  const [progressValue, setProgressValue] = useState(0);
+
+  const [previousDispatchedEvent, setPreviousDispatchedEvent] = useState(null);
+
+  const handleScroll = useCallback(() => setScrollPosition(window.pageYOffset), []);
 
   useEffect(() => {
     if (sectionRef.current) {
@@ -21,12 +25,41 @@ const Bees = () => {
       setAnimationStartPosition(animationStartPosition);
       setAnimationEndPosition(animationStartPosition + window.innerHeight);
     }
-  }, []);
+  }, [sectionRef]);
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll, { passive: true });
+    setProgressValue(
+      ((scrollPosition - animationStartPosition) /
+        (animationEndPosition - animationStartPosition)) *
+        100
+    );
+  }, [scrollPosition, animationStartPosition, animationEndPosition]);
+
+  useEffect(() => {
+    if (progressValue <= 50) {
+      if (previousDispatchedEvent !== 'ebpf-bee-trigger-in-animation') {
+        document.dispatchEvent(new Event('ebpf-bee-trigger-in-animation'));
+        setPreviousDispatchedEvent('ebpf-bee-trigger-in-animation');
+      }
+    } else if (previousDispatchedEvent !== 'ebpf-bee-trigger-out-animation') {
+      document.dispatchEvent(new Event('ebpf-bee-trigger-out-animation'));
+      setPreviousDispatchedEvent('ebpf-bee-trigger-out-animation');
+    }
+  }, [progressValue, previousDispatchedEvent]);
+
+  useEffect(() => {
+    if (sectionRef.current) sectionRefFunction(sectionRef.current);
+  }, [sectionRefFunction]);
+
+  useEffect(() => {
+    if (isSectionInView) {
+      window.addEventListener('scroll', handleScroll, { passive: true });
+    } else {
+      window.removeEventListener('scroll', handleScroll);
+    }
+
     return () => window.removeEventListener('scroll', handleScroll);
-  }, [handleScroll]);
+  }, [isSectionInView, handleScroll]);
 
   return (
     <div className="relative h-[calc(200vh+400px)]" ref={sectionRef}>
@@ -35,11 +68,7 @@ const Bees = () => {
         <div
           className="absolute top-0 left-0 right-0 bottom-0"
           style={{
-            clip: `rect(0, ${
-              ((scrollPosition - animationStartPosition) /
-                (animationEndPosition - animationStartPosition)) *
-              100
-            }vw, 100vh, 0)`,
+            clip: `rect(0, ${progressValue}vw, 100vh, 0)`,
           }}
         >
           <Features />
