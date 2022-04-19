21b4949a0f6189a60f781448a5c316c58b49fc5a
Maksim Gorodov <silencer.web@gmail.com>
2022-04-19 15:47:18 +0400

    fix: remove bees from the component Bees

--- src/components/pages/home/bees/bees.jsx
+++ src/components/pages/home/bees/bees.jsx
@@ -3,21 +3,17 @@ import React, { useCallback, useEffect, useRef, useState } from 'react';
 import Features from '../features';
 import ProminentText from '../prominent-text';
 
-import bees from './images/bees.svg';
-
 const Bees = () => {
   const sectionRef = useRef(null);
 
   const [scrollPosition, setScrollPosition] = useState(0);
-  const [scrollDirection, setScrollDirection] = useState();
 
   const [animationStartPosition, setAnimationStartPosition] = useState(0);
   const [animationEndPosition, setAnimationEndPosition] = useState(0);
 
   const handleScroll = useCallback(() => {
-    setScrollDirection(scrollPosition > window.scrollY ? 'up' : 'down');
     setScrollPosition(window.pageYOffset);
-  }, [scrollPosition]);
+  }, []);
 
   useEffect(() => {
     if (sectionRef.current) {
@@ -51,19 +47,6 @@ const Bees = () => {
         >
           <Features />
         </div>
-        <img
-          className="absolute top-1/2"
-          src={bees}
-          alt=""
-          style={{
-            left: `${
-              ((scrollPosition - animationStartPosition) /
-                (animationEndPosition - animationStartPosition)) *
-              100
-            }vw`,
-            transform: `translate(-50%, -50%) rotate(${scrollDirection === 'up' ? '180' : '0'}deg)`,
-          }}
-        />
       </div>
     </div>
   );
