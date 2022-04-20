7a6503c778a5c0ed595b9da4af214c7f53fb8793
Thi Van Le <vannnyle@gmail.com>
2022-04-20 15:49:55 +0300

    fix: embed svg

--- src/components/pages/home/code-snippets/code-snippets.jsx
+++ src/components/pages/home/code-snippets/code-snippets.jsx
@@ -1,49 +1,49 @@
 import { useAnimation } from 'framer-motion';
-import React, { Fragment, useEffect, useMemo } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import { useInView } from 'react-intersection-observer';
 
+// import frame1 from './images/frame.svg'
 import CPlusplus from './snippets/c-plusplus';
 import CPlusplusInactive from './snippets/c-plusplus-inactive';
 import Node from './snippets/node';
 import NodeInactive from './snippets/node-inactive';
 // import Rust from './snippets/rust';
-
 const OPACITY_DURATION = 0.2;
 
-const activeVariants = {
-  initial: () => ({
-    opacity: 0,
-  }),
-  animate: ({ delay }) => ({
-    opacity: 1,
-    transition: {
-      delay: delay.opacity,
-      duration: 0.1,
-    },
-  }),
-  exit: () => ({
-    opacity: 0,
-  }),
-};
+// const activeVariants = {
+//   initial: () => ({
+//     opacity: 0,
+//   }),
+//   animate: ({ delay }) => ({
+//     opacity: 1,
+//     transition: {
+//       delay: delay.opacity,
+//       // duration: 0.1,
+//     },
+//   }),
+//   exit: () => ({
+//     opacity: 0,
+//   }),
+// };
 
-const inactiveVariants = {
-  initial: () => ({
-    opacity: 0,
-  }),
-  animate: ({ delay }) => ({
-    opacity: 1,
-    transition: {
-      delay: delay.opacity,
-      // duration: 0.2,
-    },
-  }),
-};
+// const inactiveVariants = {
+//   initial: () => ({
+//     opacity: 0,
+//   }),
+//   animate: ({ delay }) => ({
+//     opacity: 1,
+//     transition: {
+//       delay: delay.opacity,
+//       // duration: 0.2,
+//     },
+//   }),
+// };
 
 const snippets = [
   {
     active: {
       snippet: CPlusplus,
-      className: 'absolute top-1/2 left-1/2 w-[56%] translate-y-[-60%] translate-x-[-70%]',
+      className: 'absolute top-1/2 left-1/2 w-[57%] translate-y-[-60%] translate-x-[-69.5%]',
     },
     inactive: {
       snippet: CPlusplusInactive,
@@ -109,7 +109,7 @@ const CodeSnippets = () => {
   }, [inView, snippetControls]);
   return (
     <div ref={ref} className="relative h-screen overflow-hidden">
-      {snippetsWithAnimationData.map(({ active, inactive }, index) => {
+      {/* {snippetsWithAnimationData.map(({ active, inactive }, index) => {
         const ActiveSnippet = active.snippet;
         const InactiveSnippet = inactive.snippet;
         return (
@@ -118,7 +118,15 @@ const CodeSnippets = () => {
             <InactiveSnippet {...inactive} controls={snippetControls} variants={inactiveVariants} />
           </Fragment>
         );
-      })}
+      })} */}
+      <img
+        className="w-full"
+        src="data:image/svg+xml;charset=utf-8,%3Csvg width='1731' height='857' xmlns='http://www.w3.org/2000/svg' version='1.1'%3E%3C/svg%3E"
+        alt=""
+        aria-hidden="true"
+      />
+      <CPlusplus className="absolute left-0 top-0 h-full w-full" />
+      {/* <embed className="absolute top-0 left-0 h-full w-full" src={frame1} alt="" /> */}
     </div>
   );
 };
