033783e3fb9e821004ba0b0e427485d7084ab96a
Maksim Gorodov <silencer.web@gmail.com>
2022-04-21 17:55:29 +0400

    feat: set icons as emojis

--- src/components/pages/home/tools/tools.jsx
+++ src/components/pages/home/tools/tools.jsx
@@ -1,5 +1,4 @@
 import clsx from 'clsx';
-import { StaticImage } from 'gatsby-plugin-image';
 import React from 'react';
 
 import BracesIcon from './images/braces.inline.svg';
@@ -10,19 +9,7 @@ const Tools = () => (
     <div className="container flex h-full items-center justify-center">
       <div className="relative">
         <h2 className={clsx('heading-6xl flat-none lg:flat-breaks')}>
-          Broken down{' '}
-          <StaticImage
-            className="inline-flex justify-center align-middle lg:h-12 md:h-10"
-            src="./images/icons.png"
-            lazy="loading"
-            quality={95}
-            objectFit="contain"
-            formats={['png']}
-            width={184}
-            height={56}
-            alt="Icons"
-          />{' '}
-          by method name, class name, and{' '}
+          Broken down 🛠👀🛠 by method name, class name, and{' '}
           <BracesIcon className="mb-1.5 inline-flex h-14 w-14 items-center xl:h-12 xl:w-12 lg:h-10 lg:w-10 md:h-7 md:w-7" />{' '}
           line number. Without complex overhead, in{' '}
           <InfinityIcon className="mb-1.5 inline-flex h-14 w-14 items-center xl:h-12 xl:w-12 lg:h-10 lg:w-10 md:h-7 md:w-7" />{' '}
