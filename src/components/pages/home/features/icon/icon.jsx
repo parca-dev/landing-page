ee703205d0ecd82f215698174ece042e267b5c0d
Thi Van Le <vannnyle@gmail.com>
2022-04-26 16:19:36 +0300

    feat: add rive animation of feature icons

---
+++ src/components/pages/home/features/icon/icon.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+import React, { useEffect } from 'react';
+import { useRive } from 'rive-react';
+
+const Icon = ({ isSectionInView, iconName }) => {
+  const { RiveComponent, rive } = useRive({
+    src: '/animations/pages/features/icons.riv',
+    stateMachines: 'State Machine',
+    autoplay: false,
+    animations: iconName,
+    artboard: iconName,
+  });
+
+  useEffect(() => {
+    if (rive) {
+      if (isSectionInView) {
+        rive.play(['State Machine']);
+      }
+    }
+  }, [isSectionInView, rive]);
+
+  return <RiveComponent className="h-[88px] w-[88px] md:h-20 md:w-20 sm:h-16 sm:w-16" />;
+};
+
+Icon.propTypes = {
+  isSectionInView: PropTypes.bool.isRequired,
+  iconName: PropTypes.string.isRequired,
+};
+
+Icon.defaultProps = {};
+
+export default Icon;
