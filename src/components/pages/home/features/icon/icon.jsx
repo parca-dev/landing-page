14195f3468e4e99d3df0c410ce48406b57881d30
Thi Van Le <vannnyle@gmail.com>
2022-05-04 15:01:02 +0200

    fix: play icon animations when inView

--- src/components/pages/home/features/icon/icon.jsx
+++ src/components/pages/home/features/icon/icon.jsx
@@ -5,18 +5,15 @@ import { useRive } from 'rive-react';
 const Icon = ({ isSectionInView, iconName }) => {
   const { RiveComponent, rive } = useRive({
     src: '/animations/pages/features/icons.riv',
-    autoplay: false,
     animations: iconName,
+    autoplay: false,
     artboard: iconName,
   });
-
   useEffect(() => {
-    if (rive) {
-      if (isSectionInView) {
-        rive.play();
-      }
+    if (isSectionInView && rive) {
+      rive.play([iconName]);
     }
-  }, [isSectionInView, rive]);
+  }, [isSectionInView, rive, iconName]);
 
   return <RiveComponent className="h-[88px] w-[88px] md:h-20 md:w-20 sm:h-16 sm:w-16" />;
 };
