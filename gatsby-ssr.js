c0d3f3e95c975b63daca517cbcb9e461f00cc2de
Alex Barashkov <lnikell@gmail.com>
2022-05-12 14:16:00 +0200

    chore: add plausible

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,34 @@
  */
 
 // You can delete this file if you're not using it
+
+import * as React from 'react';
+
+const SITE_DOMAIN = `parca.dev`;
+const PLAUSIBLE_DOMAIN = `plausible.io`;
+const SCRIPT_URI = `/js/plausible.js`;
+
+// eslint-disable-next-line import/prefer-default-export
+export const onRenderBody = ({ setHeadComponents }) => {
+  if (process.env.NODE_ENV === `production`) {
+    const scriptProps = {
+      'data-domain': SITE_DOMAIN,
+      src: `https://${PLAUSIBLE_DOMAIN}${SCRIPT_URI}`,
+    };
+
+    return setHeadComponents([
+      <link key="plausible-preconnect" rel="preconnect" href={`https://${PLAUSIBLE_DOMAIN}`} />,
+      <script key="plausible-script" defer {...scriptProps} />,
+      // See: https://plausible.io/docs/custom-event-goals#1-trigger-custom-events-with-javascript-on-your-site
+      <script
+        key="plausible-custom-events"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) };
+          `,
+        }}
+      />,
+    ]);
+  }
+  return null;
+};
