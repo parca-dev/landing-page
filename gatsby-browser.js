d4570d11be4e252247e86fbff30165f602498446
Manoj Vivek <p.manoj.vivek@gmail.com>
2023-03-01 12:42:09 +0530

    Removed custom plausible events

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,10 +6,3 @@
 
 // You can delete this file if you're not using it
 import './src/styles/main.css';
-
-// eslint-disable-next-line import/prefer-default-export
-export const onRouteUpdate = () => {
-  if (process.env.NODE_ENV === `production` && typeof window.plausible !== `undefined`) {
-    window.plausible(`pageview`);
-  }
-};
