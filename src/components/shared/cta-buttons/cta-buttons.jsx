99d3606de836183c1edd72e22e4020d6077a5a1d
monicawoj <monica.wojciechowski@polarsignals.com>
2023-08-02 13:42:50 +0200

    feat: add discord link to nav bar

--- src/components/shared/cta-buttons/cta-buttons.jsx
+++ src/components/shared/cta-buttons/cta-buttons.jsx
@@ -3,41 +3,43 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 import LINKS from 'constants/links';
+import DiscordLogo from 'icons/discord.inline.svg';
 import GithubLogo from 'icons/github.inline.svg';
 
 import Button from '../button';
 
 const buttons = [
+  { logo: DiscordLogo, text: 'Join us on Discord', to: LINKS.discord },
   { logo: GithubLogo, text: 'Star Us', to: LINKS.github },
-  { text: 'Try it Now', to: LINKS.demo },
+  { text: 'Try it Now', to: LINKS.demo, isPrimary: true },
 ];
 
 const CtaButtons = ({ className, isBlackBg }) => (
   <div className={clsx('flex items-center space-x-5', className)}>
-    {buttons.map(({ logo: Logo, text, to }, index) => {
+    {buttons.map(({ logo: Logo, text, to, isPrimary }, index) => {
       let theme = '';
 
-      if (index === 0) {
+      if (isPrimary) {
         if (isBlackBg) {
-          theme = 'white-outline';
+          theme = 'white-filled';
         } else {
-          theme = 'black-outline';
+          theme = 'black-filled';
         }
-      } else if (!isBlackBg) {
-        theme = 'black-filled';
+      } else if (isBlackBg) {
+        theme = 'white-outline';
       } else {
-        theme = 'white-filled';
+        theme = 'black-outline';
       }
 
       return (
         <Button
-          className={clsx('relative justify-center md:w-full sm:w-auto', Logo && 'pl-11')}
+          className={clsx('flex items-center justify-center')}
           to={to}
           theme={theme}
           size="sm"
           key={index}
         >
-          {Logo && <Logo className="absolute top-1/2 left-2.5 h-6 w-auto -translate-y-1/2" />}
+          {Logo && <Logo className="h-6 w-auto pr-3" />}
           <span>{text}</span>
         </Button>
       );
